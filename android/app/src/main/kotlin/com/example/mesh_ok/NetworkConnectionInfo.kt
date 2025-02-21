785c1a0df4801608e28e40d73deac79df568f0f9
Юджин Гофлаттер <yuflutter@gmail.com>
2025-02-21 18:09:02 +0300

    dev

---
+++ android/app/src/main/kotlin/com/example/mesh_ok/NetworkConnectionInfo.kt
@@ -0,0 +1,9 @@
+package com.example.mesh_ok
+
+import android.net.wifi.WifiInfo
+import android.net.wifi.p2p.WifiP2pInfo
+
+class NetworkInfo(
+    val wifiP2pInfo: WifiP2pInfo,
+    val wifiInfo: WifiInfo,
+)
